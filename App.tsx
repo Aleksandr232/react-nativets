560434e9d1b8bc7e1238dad2e548fa4157ba7b26
Aleksandr232 <aleksrowing232@gmail.com>
2022-07-13 07:02:01 +0300

    status-bar

--- App.tsx
+++ App.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, ImageBackground, useWindowDimensions, StatusBar } from 'react-native';
 
 export default function App() {
+  const {width: windowWidth, height:windowHeight}=useWindowDimensions()
   return (
+    <>
+    <StatusBar barStyle='light-content'/>
     <ScrollView>
-
+      <View style={{width: windowWidth, height: windowHeight}}>
+          <ImageBackground source={require('./assets/night2.jpg')} style={{flex:1}}/>
+      </View>
     </ScrollView>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
+/* const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+}); */
