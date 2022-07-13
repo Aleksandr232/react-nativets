a2a3eb1e8efcc6b3a2c6f259dd51786f8bca3884
Aleksandr232 <aleksrowing232@gmail.com>
2022-07-13 07:31:27 +0300

    location

--- App.tsx
+++ App.tsx
@@ -1,15 +1,40 @@
 import React from 'react';
 import { StyleSheet, Text, View, ScrollView, ImageBackground, useWindowDimensions, StatusBar } from 'react-native';
+import Locations from './model/location';
 
-export default function App() {
+
+interface bgImgs{
+  bgImg: object
+}
+
+
+export default function App({bgImg}: bgImgs) {
   const {width: windowWidth, height:windowHeight}=useWindowDimensions()
   return (
     <>
     <StatusBar barStyle='light-content'/>
-    <ScrollView>
-      <View style={{width: windowWidth, height: windowHeight}}>
-          <ImageBackground source={require('./assets/night2.jpg')} style={{flex:1}}/>
-      </View>
+    <ScrollView 
+      horizontal={true}
+      pagingEnabled
+      showsHorizontalScrollIndicator={false}
+    >
+      {Locations.map((location, index)=>{
+         if (location.weatherType == 'Sunny') {
+           bgImg = require('./assets/sunny.jpg');
+        } else if (location.weatherType == 'Night') {
+           bgImg = require('./assets/night2.jpg');
+        } else if (location.weatherType == 'Cloudy') {
+          bgImg = require('./assets/cloudy.jpeg');
+        } else if (location.weatherType == 'Rainy') {
+           bgImg = require('./assets/rainy.jpg');
+        }
+          return(
+          <View style={{width: windowWidth, height: windowHeight}} key={index}>
+            <ImageBackground source={bgImg} style={{flex:1}}/>
+        </View>
+          );
+      })}
+     
     </ScrollView>
     </>
   );
